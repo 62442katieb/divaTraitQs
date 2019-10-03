db73cfde421bd0592f8b235051a663d8ea475c17
Katie Bottenhorn <kbott006@fiu.edu>
2019-10-03 10:40:39 -0400

    add state readme

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ export default {
   // githubSrc: 'https://raw.githubusercontent.com/ReproNim/schema-standardization/master/activity-sets/voice-pilot/voice_pilot_schema.jsonld',
   // githubSrc: 'https://raw.githubusercontent.com/ReproNim/schema-standardization/master/activity-sets/ema-hbn/ema-hbn_schema.jsonld',
   githubSrc: 'https://raw.githubusercontent.com/62442katieb/diva-questionnaires/master/activitySets/divaTraitActivitySet/divaTraitActivitySet_schema.jsonld',
-  contentSrc: 'https://raw.githubusercontent.com/ReproNim/schema-standardization/master/activity-sets/voice-pilot/README.md'
+  contentSrc: 'https://raw.githubusercontent.com/62442katieb/diva-questionnaires/master/activitySets/divaTraitActivitySet/divaTraitActivitySetReadMe.Md'
 };
